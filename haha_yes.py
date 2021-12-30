d95dc53d93af7366d1a9df5e31aa2524f56f5453
magmastuff <70583916+MagmaStuff@users.noreply.github.com>
2021-12-30 21:50:17 +0530

    Add files via upload

---
+++ haha_yes.py
@@ -0,0 +1,40 @@
+import time
+
+
+def main():
+    print("Checking user profile")
+    time.sleep(2)
+    print("Checking networth")
+    time.sleep(2)
+    print("10% done")
+    time.sleep(1)
+    print("18% done")
+    time.sleep(1)
+    print("32% done")
+    time.sleep(1)
+    print("54% done")
+    time.sleep(1)
+    print("88% done")
+    time.sleep(1)
+    print("100% done Succesful!")
+    time.sleep(4)
+
+    print("your hyperion has been generated!")
+    time.sleep(2)
+    print("Its a Chimera V! YOUR LUCKY!")
+    time.sleep(1)
+    print("you now need to go to this url to get your reward:\n https://www.shorturl.at/akqNZ")
+    time.sleep(5)
+    print("rip u got rickrolled, share this with your friends and rickroll them!\n made my magmastuff")
+    exit()
+
+print("welcome to the hyperion generator.")
+time.sleep(0.5)
+ign = input("to understand you are a actual hypixel skyblock player,\n please enter your IGN(ingame name): ")
+if(ign == ""):
+    print("error while processing!")
+    exit()
+else:
+    main()
+
+
